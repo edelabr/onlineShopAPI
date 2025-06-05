ecaea45dbe91028d30e5b0f57fecf4697debab0b
Enrique de la Brena Sevillano <enrique.de_la_brena_sevillano@mercedes-benz.com>
2025-06-05 17:49:37 +0200

    feat: add cache with redis

--- app/clients/dummy_json_client.py
+++ app/clients/dummy_json_client.py
@@ -1,16 +1,33 @@
+import json
 import requests
+from app.db.redis_client import redis_client
 import urllib3
 
 # Desactiva advertencias por verificación SSL (¡solo para desarrollo!)
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-def get_products(skip: int = 0, limit: int = 0):
+# Tiempo de vida en caché (en segundos)
+CACHE_TTL = 600  # 10 minutos
+
+async def get_products(skip: int = 0, limit: int = 0, product_id: int = None):
     try:
+        if product_id is not None:
+            cache_key = f"product_{product_id}"
+            url = f"https://dummyjson.com/products/{product_id}"
+        else:
+            cache_key = f"products_skip_{skip}_limit{limit}"
+            url = f"https://dummyjson.com/products?skip={skip}&limit={limit}"
+    
+        # Verificar si el producto ya está en caché
+        cached_product = await redis_client.get(cache_key)
+        if cached_product:
+            return json.loads(cached_product)
+        
         url = f"https://dummyjson.com/products?skip={skip}&limit={limit}"
         response = requests.get(url, verify=False)
         if response.status_code == 200:
             products = response.json().get("products", [])
-            return [
+            data = [
                 {
                     "id": p["id"],
                     "title": p["title"],
@@ -19,29 +36,55 @@ def get_products(skip: int = 0, limit: int = 0):
                 }
                 for p in products
             ]
+
+            if product_id:
+                returned_data = data[0]
+            else:
+                returned_data = data
+
+            # Guardar en Redis con TTL
+            await redis_client.setex(cache_key, CACHE_TTL, json.dumps(returned_data))
+
+            return returned_data
         else:
             print(f"DummyJSON API response error: {response.status_code}")
     except Exception as e:
         print(f"DummyJSON API error: {e}")
     return None
 
-def get_product_by_id(product_id: int):
+async def get_product_by_id(product_id: int):
     try:
-        products = get_products()
+        cache_key = f"product_{product_id}"
+    
+        # Verificar si el producto ya está en caché
+        cached_product = await redis_client.get(cache_key)
+        if cached_product:
+            return json.loads(cached_product)
+        
+        products = await get_products()
         filtered_products = list(filter(lambda p: p["id"] == product_id, products))
 
         if filtered_products:
+            await redis_client.setex(cache_key, CACHE_TTL, json.dumps(filtered_products[0]))
             return filtered_products[0]
         print(f"No product found with id: {product_id}")
     except Exception as e:
         print(f"DummyJSON API error: {e}")
     return None
 
-def get_product_by_name(product_name: str):
+async def get_product_by_name(product_name: str):
     try:
-        products = get_products()
+        cache_key = f"product_{product_name}"
+    
+        # Verificar si el producto ya está en caché
+        cached_product = await redis_client.get(cache_key)
+        if cached_product:
+            return json.loads(cached_product)
+        
+        products = await get_products()
         filtered_products = list(filter(lambda p: p["title"] == product_name, products))
         if filtered_products:
+            await redis_client.setex(cache_key, CACHE_TTL, json.dumps(filtered_products[0]))
             return filtered_products[0]
         print(f"No product found with name: {product_name}")
     except Exception as e:
--- app/routes/product.py
+++ app/routes/product.py
@@ -6,15 +6,15 @@ from app.clients.dummy_json_client import get_products, get_product_by_name
 router = APIRouter(prefix="/products", tags=["products"])
 
 @router.get("/")
-def list_products(skip: int = 0, limit: int = 10):
-    products = get_products(skip, limit)
+async def list_products(skip: int = 0, limit: int = 10):
+    products = await get_products(skip, limit)
     if products is None:
         return JSONResponse(status_code=502, content={"detail": "Failed to fetch products"})
     return products
 
 @router.get("/{product_name}")
-def list_products_by_name(product_name: str):
-    products = get_product_by_name(product_name)
+async def list_products_by_name(product_name: str):
+    products = await get_product_by_name(product_name)
     if products is None:
         return JSONResponse(status_code=502, content={"detail": "Failed to fetch products"})
     return products
